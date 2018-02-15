9881341d824568fdb1d29241b46d9b5b0c10a777
danielnutu777 <danielnutu23@gmail.com>
2018-02-15 22:46:44 +0200

    added moreexercises

--- src/main/java/basics/Ex32.java
+++ src/main/java/basics/Ex32.java
@@ -1,8 +1,21 @@
 package basics;
 
+import java.util.Scanner;
+
 public class Ex32 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Input first integer: ");
+        int firstNo = scanner.nextInt();
+
+        System.out.print("Input second integer: ");
+        int secondNo = scanner.nextInt();
+        if (firstNo != secondNo && firstNo <= secondNo) {
+            System.out.println(firstNo + " != " + secondNo);
+            System.out.println(firstNo + " < " + secondNo);
+            System.out.println(firstNo + " <= " + secondNo);
+        }
     }
 }
 /*32. Write a Java program to compare two numbers.
