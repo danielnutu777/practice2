68e727344a886b6120b97e921204e850ffa838ff
danielnutu777 <danielnutu23@gmail.com>
2018-02-16 17:18:07 +0200

    added one ex

---
+++ src/main/java/basics/Ex37.java
@@ -0,0 +1,22 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Ex37 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input a string: ");
+
+        String s = scanner.next();
+
+        StringBuilder sb = new StringBuilder();
+        
+    }
+}
+/*37. Write a Java program to reverse a string.
+Input Data:
+Input a string: The quick brown fox
+Expected Output
+
+Reverse string: xof nworb kciuq ehT*/
