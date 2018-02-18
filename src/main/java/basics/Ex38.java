0e4f4773008bcbdf5c0a5d37779c2da11e17c241
danielnutu777 <danielnutu23@gmail.com>
2018-02-18 23:59:01 +0200

    added moreexercises

--- src/main/java/basics/Ex38.java
+++ src/main/java/basics/Ex38.java
@@ -5,7 +5,30 @@ import java.util.Scanner;
 public class Ex38 {
     public static void main(String[] args) {
         Scanner scanner =  new Scanner(System.in);
+
+        System.out.print("The string is: ");
         String s = scanner.nextLine();
+
+        int letterCounter = 0;
+        int spaceCounter = 0;
+        int noCounter = 0;
+        int otherCharCounter = 0;
+
+        char temp;
+        for (int i = 0; i < s.length(); i++){
+            temp = s.charAt(i);
+            if (Character.isDigit(temp)){
+                noCounter += 1;
+            }else if (Character.isLetter(temp)) {
+                letterCounter += 1;
+            }else if (Character.isSpaceChar(temp)){
+                spaceCounter += 1;
+            }else{
+                otherCharCounter += 1;
+            }
+        }
+        System.out.print("Letter: " + letterCounter + "\n" + "Spaces: " + spaceCounter + "\n" + "Numbers: " + noCounter + "\n" + "Others: " + otherCharCounter + "\n");
+
     }
 }
 /*38. Write a Java program to count the letters, spaces, numbers and other characters of an input string.
---
+++ src/main/java/basics/Ex41.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Ex41 {
+    public static void main(String[] args) {
+        int chr = 'Z';
+        System.out.println("The ASCII value of Z is :"+chr);
+    }
+}
+/*41. Write a Java program to print the ascii value of a given character.
+Expected Output
+
+The ASCII value of Z is : 90*/
