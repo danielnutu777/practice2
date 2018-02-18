94654ac9fee7f86828cbfcee048281a7e20b073b
danielnutu777 <danielnutu23@gmail.com>
2018-02-18 12:35:22 +0200

    added moreexercises

--- src/main/java/basics/Ex37.java
+++ src/main/java/basics/Ex37.java
@@ -6,12 +6,18 @@ public class Ex37 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+//        System.out.print("Input a string: ");
+//        StringBuilder sb = new StringBuilder(scanner.nextLine());
+//
+//        System.out.println("Reverse string: " + sb.reverse().toString());
+
         System.out.print("Input a string: ");
-
-        String s = scanner.next();
-
-        StringBuilder sb = new StringBuilder();
-        
+        char[] letters = scanner.nextLine().toCharArray();
+        System.out.print("Reverse string: ");
+        for (int i = letters.length - 1; i >= 0; i--) {
+            System.out.print(letters[i]);
+        }
+        System.out.print("\n");
     }
 }
 /*37. Write a Java program to reverse a string.
---
+++ src/main/java/basics/Ex38.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Ex38 {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        String s = scanner.nextLine();
+    }
+}
+/*38. Write a Java program to count the letters, spaces, numbers and other characters of an input string.
+Expected Output
+
+The string is :  Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33
+letter: 23
+space: 9
+number: 10
+other: 6*/
