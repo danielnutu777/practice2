3ae9b93dcd8341b028572420f9b8907a1c9e5ff8
danielnutu777 <danielnutu23@gmail.com>
2018-02-19 17:30:59 +0200

    added one ex

---
+++ src/main/java/basics/Ex42.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Ex42 {
+    public static void main(String[] args) {
+        
+    }
+}
+/*42. Write a Java program to input and display your password.
+Expected Output
+
+Input your Password:
+Your password was: abc@123*/
