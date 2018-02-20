f4a3b386c33bd582d2cc195b22bec77046c22ef9
danielnutu777 <danielnutu23@gmail.com>
2018-02-20 23:04:25 +0200

    ex50

--- src/main/java/basics/Ex55.java
+++ src/main/java/basics/Ex55.java
@@ -2,7 +2,9 @@ package basics;
 
 public class Ex55 {
     public static void main(String[] args) {
-        
+        if(true){
+            
+        }
     }
 }
 /*55. Write a Java program to convert seconds to hour, minute and seconds.
