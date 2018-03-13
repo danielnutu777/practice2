5c9ac8441c1ee02115dcf76509f6b02ba9a269dc
danielnutu777 <danielnutu23@gmail.com>
2018-03-13 23:39:01 +0200

    added more ex

--- src/main/java/conditionals/Ex1.java
+++ src/main/java/conditionals/Ex1.java
@@ -1,7 +1,26 @@
 package conditionals;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int input = scanner.nextInt();
+
+
+        for (int i = 1; i <= input; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("\t" + i + "\t");
+            }
+            System.out.println();
+        }
     }
 }
+/*19. Write a program in Java to make such a pattern like a pyramid with a number which will repeat the number in the same row. Go to the editor
+
+       1
+      2 2
+     3 3 3
+    4 4 4 4
+Click me to see the solution */
